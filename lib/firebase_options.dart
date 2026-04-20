c3b7d4e06cf68b869faced1ba4875dc5ebfa8e69
Bigg PAPA <nito03@icloud.com>
2026-04-20 18:53:22 -0400

    Firebase added to base project

---
+++ lib/firebase_options.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'Web is not configured. Run flutterfire configure again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'This platform is not configured. Run flutterfire configure again.',
+        );
+      default:
+        throw UnsupportedError(
+          'This platform is not supported.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'PASTE_ANDROID_API_KEY_HERE',
+    appId: 'PASTE_ANDROID_APP_ID_HERE',
+    messagingSenderId: 'PASTE_MESSAGING_SENDER_ID_HERE',
+    projectId: 'PASTE_PROJECT_ID_HERE',
+    storageBucket: 'PASTE_STORAGE_BUCKET_HERE',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'PASTE_IOS_API_KEY_HERE',
+    appId: 'PASTE_IOS_APP_ID_HERE',
+    messagingSenderId: 'PASTE_MESSAGING_SENDER_ID_HERE',
+    projectId: 'PASTE_PROJECT_ID_HERE',
+    storageBucket: 'PASTE_STORAGE_BUCKET_HERE',
+    iosBundleId: 'PASTE_IOS_BUNDLE_ID_HERE',
+  );
+}
