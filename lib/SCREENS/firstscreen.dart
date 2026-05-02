27e41bf9668a079cfd9c260e16bb62f74f8d6222
Hien Dao <hienqdao5@gmail.com>
2026-05-02 02:49:51 -0400

    Update firstscreen.dart to have appbar title and animated text

--- lib/SCREENS/firstscreen.dart
+++ lib/SCREENS/firstscreen.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
-class firstscreen extends StatelessWidget {
-  const firstscreen({super.key});
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        title: const Text(
+          'Welcome to TropicaGuide!',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-         const Text(
-          'Welcome',
-          style: TextStyle(fontSize: 24),
-        ),
-        const SizedBox(height: 20),
-        //Go to login via /login
-        ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text('Login'),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'Ready to start your adventure?',
+                  textStyle: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  speed: const Duration(milliseconds: 100),
+                ),
+              ],
+              totalRepeatCount: 1,
             ),
-        //Go to create account via /create_account  
-        ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/createaccount');
-              },
-              child: const Text('Create Account'),
-            ),
+          ),
+          const SizedBox(height: 20),
+
+          //Go to create account via /create_account  
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/createaccount');
+            },
+            child: const Text('Create Account'),
+          ),
+
+          //Go to login via /login
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: const Text('Login'),
+          ),
         ],
-        ),
       ),
     );
   }
